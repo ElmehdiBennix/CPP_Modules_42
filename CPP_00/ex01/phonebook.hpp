73c28c5ea30e9bf74b008daf13b8eb08a7b266f4
ElmehdiBennix <bennixmehdi@gmail.com>
2023-10-31 02:55:04 +0100

    AutoPush

--- CPP_00/ex01/phonebook.hpp
+++ CPP_00/ex01/phonebook.hpp
@@ -6,7 +6,7 @@
 /*   By: ebennix <ebennix@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/30 20:47:30 by ebennix           #+#    #+#             */
-/*   Updated: 2023/10/30 20:47:50 by ebennix          ###   ########.fr       */
+/*   Updated: 2023/10/31 00:12:19 by ebennix          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,9 @@
 #define PHONEBOOK_HPP
 
 #include <iostream>
+
+#define MAX_CONTACTS 8
+#define string std::string
 
 
 #endif
