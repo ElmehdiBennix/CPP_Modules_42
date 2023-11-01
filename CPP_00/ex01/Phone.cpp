5a4e7f2b54f8c8bf7d36478844698d42dfec7fb3
ElmehdiBennix <bennixmehdi@gmail.com>
2023-11-01 21:36:36 +0100

    AutoPush

---
+++ CPP_00/ex01/Phone.cpp
@@ -0,0 +1,39 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Phone.cpp                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ebennix <ebennix@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/10/30 20:47:27 by ebennix           #+#    #+#             */
+/*   Updated: 2023/11/01 21:23:56 by ebennix          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Phonebook.hpp"
+
+int main(void)
+{
+    PhoneBook phonebook;
+    
+    string command;
+    std::cout << "      -> Welcome to the crappy phonebook <-      " << std::endl;
+    std::cout << "##################################################" << std::endl;
+
+    while (true)
+    {
+        std::cout << "Enter a command 'ADD' or 'SEARCH' or 'EXIT': ";
+        if (!std::getline(std::cin, command) || command == "EXIT")
+            exit_prog(0);
+        else if (command == "ADD")
+            phonebook.add();
+        else if (command == "SEARCH")
+            phonebook.search();
+        else
+            std::cout << "-> Invalid command." << std::endl;
+    }
+    return (0);
+
+}
+
+// DEFINE COLORS
