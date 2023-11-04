2f752859db4097cb72c9faa9d3ad79677954cf75
ElmehdiBennix <bennixmehdi@gmail.com>
2023-11-04 12:59:18 +0100

    AutoPush

--- CPP_01/ex01/main.cpp
+++ CPP_01/ex01/main.cpp
@@ -6,19 +6,32 @@
 /*   By: ebennix <ebennix@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/03 04:19:16 by ebennix           #+#    #+#             */
-/*   Updated: 2023/11/04 11:48:58 by ebennix          ###   ########.fr       */
+/*   Updated: 2023/11/04 12:59:08 by ebennix          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Zombie.hpp"
 
+// void leaks(void)
+// {
+//     system("leaks Moar_brainz");
+// }
+
 int main (void)
 {
-    std::cout << "**************************************** " << std::endl;
+    int N = 5;
+    Zombie *ptr = zombieHorde(N, "el gato");
 
-    std::cout << "**************************************** " << std::endl;
+    std::cout << "**********************************" << std::endl;
 
+    for (int i = 0; i < N; i++)
+        ptr[i].announce();
+    
     std::cout << "**************************************** " << std::endl;
-
-    std::cout << "**************************************** " << std::endl;
+    
+    delete[] ptr;
+    // atexit(leaks);
 }
+
+// delete is used to free allocated memory using new
+// if u wanna clear out an allocated array u have to use delete[]
