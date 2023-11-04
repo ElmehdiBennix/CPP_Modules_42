0c267905a5f22f21903a6e838e687cfa9b2cebe4
ElmehdiBennix <bennixmehdi@gmail.com>
2023-11-04 11:57:44 +0100

    AutoPush

--- CPP_01/ex01/zombieHorde.cpp
+++ CPP_01/ex01/zombieHorde.cpp
@@ -1,20 +1,18 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   newZombie.cpp                                      :+:      :+:    :+:   */
+/*   zombieHorde.cpp                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: ebennix <ebennix@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/11/03 04:19:20 by ebennix           #+#    #+#             */
-/*   Updated: 2023/11/04 11:41:25 by ebennix          ###   ########.fr       */
+/*   Created: 2023/11/04 11:48:04 by ebennix           #+#    #+#             */
+/*   Updated: 2023/11/04 11:49:30 by ebennix          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Zombie.hpp"
 
-Zombie* newZombie(std::string name)
+Zombie* zombieHorde( int N, std::string name )
 {
-    Zombie *ptr = new Zombie(name);
-    return (ptr);
+    
 }
-
