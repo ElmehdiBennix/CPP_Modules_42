40a22e1c5b90a2fa55f4a5190453ebe4e2496135
El Mehdi Bennix <ebennix@e1r12p2.1337.ma>
2023-11-04 11:57:44 +0100

    AutoPush

--- CPP_01/ex00/main.cpp
+++ CPP_01/ex00/main.cpp
@@ -6,14 +6,37 @@
 /*   By: ebennix <ebennix@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/03 04:19:16 by ebennix           #+#    #+#             */
-/*   Updated: 2023/11/03 05:37:17 by ebennix          ###   ########.fr       */
+/*   Updated: 2023/11/04 11:57:25 by ebennix          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Zombie.hpp"
 
-int main (void) {
-    
-
-    
+void leaks(void)
+{
+    system("leaks BraiiiiiiinnnzzzZ");
 }
+
+int main (void)
+{
+    randomChump("spitter");
+    std::cout << "**************************************** " << std::endl;
+    randomChump("witch");
+    std::cout << "**************************************** " << std::endl;
+    Zombie *ptr ;
+    ptr = newZombie("clicker");
+    ptr->announce();
+    delete ptr;
+    std::cout << "**************************************** " << std::endl;
+    ptr = newZombie("charger");
+    ptr->announce();
+    delete ptr;
+    std::cout << "**************************************** " << std::endl;
+    // atexit(leaks);
+}
+
+// the destructor is a special member function of a class that is automatically 
+//called when an object of that class goes out of scope or is explicitly deleted. You don't
+// need to call the destructor manually; it is called automatically by the C++ runtime system. 
+//The purpose of the destructor is to perform any necessary cleanup or resource deallocation
+// when an object's lifetime ends.
