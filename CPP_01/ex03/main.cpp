c3494f75d89157ff849ddee8a740033379dc2645
ElmehdiBennix <bennixmehdi@gmail.com>
2023-11-04 17:51:55 +0100

    AutoPush

--- CPP_01/ex03/main.cpp
+++ CPP_01/ex03/main.cpp
@@ -6,7 +6,7 @@
 /*   By: ebennix <ebennix@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/03 04:19:16 by ebennix           #+#    #+#             */
-/*   Updated: 2023/11/04 17:47:45 by ebennix          ###   ########.fr       */
+/*   Updated: 2023/11/04 17:49:53 by ebennix          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,17 +14,7 @@
 #include <string>
 
 int main() {
-    std::string     myString = "HI THIS IS BRAIN";
-    std::string*    stringPTR = &myString;
-    std::string&    stringREF = myString;
 
-    std::cout << "-> Memory address of the string variable: " << &myString << std::endl;
-    std::cout << "-> Memory address held by stringPTR: " << stringPTR << std::endl;
-    std::cout << "-> Memory address held by stringREF: " << &stringREF << std::endl;
-
-    std::cout << "-> Value of the string variable: " << myString << std::endl;
-    std::cout << "-> Value pointed to by stringPTR: " << *stringPTR << std::endl;
-    std::cout << "-> Value pointed to by stringREF: " << stringREF << std::endl;
 
     return 0;
 }
