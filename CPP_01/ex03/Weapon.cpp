8dd7c5bc9cb41ee445a52ab51979935aaadc3156
ElmehdiBennix <bennixmehdi@gmail.com>
2023-11-06 10:08:48 +0100

    AutoPush

--- CPP_01/ex03/Weapon.cpp
+++ CPP_01/ex03/Weapon.cpp
@@ -6,13 +6,13 @@
 /*   By: ebennix <ebennix@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/04 17:49:56 by ebennix           #+#    #+#             */
-/*   Updated: 2023/11/06 08:55:55 by ebennix          ###   ########.fr       */
+/*   Updated: 2023/11/06 10:06:02 by ebennix          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Weapon.hpp"
 
-const std::string   Weapon::get_Type(void) const
+std::string   Weapon::get_Type(void) 
 {
     return type;
 }
