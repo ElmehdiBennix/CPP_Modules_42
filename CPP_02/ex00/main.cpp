3cf6d552ee488547b3fc0a4b339022caee9a7ebf
ElmehdiBennix <bennixmehdi@gmail.com>
2023-12-30 04:58:33 +0100

    AutoPush

--- CPP_02/ex00/main.cpp
+++ CPP_02/ex00/main.cpp
@@ -5,14 +5,24 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: ebennix <ebennix@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/12/08 13:58:38 by ebennix           #+#    #+#             */
-/*   Updated: 2023/12/08 14:02:35 by ebennix          ###   ########.fr       */
+/*   Created: 2023/12/30 00:20:03 by ebennix           #+#    #+#             */
+/*   Updated: 2023/12/30 00:20:23 by ebennix          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "fixed.hpp"
+#include"fixed.hpp"
 
-int main ()
+int main(void)
 {
-    
+    Fixed a;
+    Fixed b(a);
+    Fixed c;
+
+    c = b;
+
+    std::cout << a.getRawBits() << std::endl;
+    std::cout << b.getRawBits() << std::endl;
+    std::cout << c.getRawBits() << std::endl;
+
+    return 0;
 }
---
+++ CPP_02/ex01/main.cpp
@@ -0,0 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ebennix <ebennix@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/12/08 13:58:38 by ebennix           #+#    #+#             */
+/*   Updated: 2023/12/30 04:21:29 by ebennix          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "fixed.hpp"
+
+int main( void ) {
+
+    Fixed       a;
+    Fixed const b( 10 );
+    Fixed const c( 42.42f );
+    Fixed const d( b );
+
+    a = Fixed( 1234.4321f );
+
+    std::cout << "a is " << a << std::endl;
+    std::cout << "b is " << b << std::endl;
+    std::cout << "c is " << c << std::endl;
+    std::cout << "d is " << d << std::endl;
+
+    std::cout << "a is " << a.toInt() << " as integer" << std::endl;
+    std::cout << "b is " << b.toInt() << " as integer" << std::endl;
+    std::cout << "c is " << c.toInt() << " as integer" << std::endl;
+    std::cout << "d is " << d.toInt() << " as integer" << std::endl;
+
+    return 0;
+}
