079765c7cbbd11e8eea31e8787b914ca0a9ddf52
El Mehdi Bennix <ebennix@e1r8p12.1337.ma>
2023-12-31 05:59:40 +0100

    AutoPush

---
+++ testing/testing0.cpp
@@ -0,0 +1,67 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   testing0.cpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ebennix <ebennix@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/12/15 21:26:14 by ebennix           #+#    #+#             */
+/*   Updated: 2023/12/31 05:59:27 by ebennix          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+#include <iostream>
+
+// testing the static data type in classes
+
+// if we want to make aclass static as in static funting in c meaning it only linkes to the current file
+// use namespace 
+//}
+    //class blud{ 
+    //}
+//}
+
+namespace {
+    class blud
+    {
+        public:
+            blud(void):x(0){
+                nemra = 9;
+                std::cout<<"constoctur called"<<std::endl;
+            };
+            void change_data(int _x, int _nemra)
+            {
+                x = _x;  
+                nemra = _nemra;
+            };
+            
+            void print_data(void) const
+            {
+                std::cout<<"this is x = "<< x <<std::endl;
+                std::cout<<"this is nemra = "<< nemra <<std::endl;
+            };
+            ~blud(void)
+            {
+                std::cout<<"destrocture called"<<std::endl;
+            };
+            
+        private:
+            int x;
+            static int nemra;
+    };
+}
+
+int blud::nemra = 0;
+
+int main(void)
+{
+    blud one;
+    blud two;
+    one.print_data();
+    one.change_data(2,4); // static variable changes in all instances of the object blud 
+    one.print_data();
+    std::cout<<"___________________________________"<<std::endl;
+    two.print_data();
+}
+
