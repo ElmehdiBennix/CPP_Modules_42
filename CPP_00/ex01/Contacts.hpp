fd63c037888757a6f8e0bd04be6d8ecc85f16ca8
El Mehdi Bennix <ebennix@e1r8p12.1337.ma>
2024-01-18 05:49:06 +0100

    AutoPush

---
+++ CPP_00/ex01/Contacts.hpp
@@ -0,0 +1,39 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Contacts.hpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ebennix <ebennix@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/11/01 20:05:30 by ebennix           #+#    #+#             */
+/*   Updated: 2024/01/18 05:48:28 by ebennix          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef CONTACTS_HPP_
+#define CONTACTS_HPP_
+
+#include <iostream>
+
+class Contacts 
+{
+    public:
+        Contacts();
+        Contacts(std::string& fname, std::string& lname, std::string& nname, std::string& nbr, std::string& secret);
+
+        void    display(void);
+        std::string  trunc_getter(int argno);
+
+    private:
+        std::string  _first_name;
+        std::string  _last_name;
+        std::string  _nickname;
+        std::string  _phone_number;
+        std::string  _darkest_secret;
+
+        std::string  trunc(const std::string& field);
+};
+
+void    exit_prog(int e_n);
+
+#endif
