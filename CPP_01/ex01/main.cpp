50ce7c7ffc29f1e4b5e4715c8ea9ee1af70d2b6a
El Mehdi Bennix <ebennix@e1r8p12.1337.ma>
2024-01-18 09:38:26 +0100

    AutoPush

--- CPP_01/ex01/main.cpp
+++ CPP_01/ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: ebennix <ebennix@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/03 04:19:16 by ebennix           #+#    #+#             */
-/*   Updated: 2023/11/04 17:54:30 by ebennix          ###   ########.fr       */
+/*   Updated: 2024/01/18 09:07:58 by ebennix          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@
 //     system("leaks Moar_brainz");
 // }
 
-int main (void)
+int main ( void )
 {
     int N = -5;
     Zombie *ptr = zombieHorde(N, "el gato");
