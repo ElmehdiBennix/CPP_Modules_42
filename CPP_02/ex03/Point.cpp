6da34731d0f8cb7b00cec6b6c059ae806d29a6d7
El Mehdi Bennix <ebennix@e1r8p12.1337.ma>
2024-01-26 08:03:57 +0100

    AutoPush

--- CPP_02/ex03/Point.cpp
+++ CPP_02/ex03/Point.cpp
@@ -6,7 +6,7 @@
 /*   By: ebennix <ebennix@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/09 07:43:22 by ebennix           #+#    #+#             */
-/*   Updated: 2024/01/09 09:44:56 by ebennix          ###   ########.fr       */
+/*   Updated: 2024/01/26 07:59:06 by ebennix          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,14 +16,15 @@ Point::Point( void ) : x(0), y(0) {};
 
 Point::Point( const float _x , const float _y): x(_x), y(_y) {};
 
-Point::Point (const Point &obj): x(obj.x), y(obj.y) {};
+Point::Point (const Point &source): x(source.x), y(source.y) {};
 
 Point& 
-Point::operator=(const Point &source){
+Point::operator=(const Point &source)
+{
     if (this != &source)
     {
-        (Fixed) x = source.x;
-        (Fixed) y = source.y;
+        (Fixed)x = source.x;
+        (Fixed)y = source.y;
     }
     return (*this);
 };
