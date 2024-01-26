7156d559c1bd40c9e087ad9cee52701a39d2c6ba
El Mehdi Bennix <ebennix@e1r8p12.1337.ma>
2024-01-26 09:39:09 +0100

    AutoPush

--- CPP_02/ex03/main.cpp
+++ CPP_02/ex03/main.cpp
@@ -6,7 +6,7 @@
 /*   By: ebennix <ebennix@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/09 07:43:06 by ebennix           #+#    #+#             */
-/*   Updated: 2024/01/26 08:03:41 by ebennix          ###   ########.fr       */
+/*   Updated: 2024/01/26 09:38:09 by ebennix          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,8 +21,13 @@ int main ( void )
     Point   target_0(1,1);
     Point   target_1(6,0);
 
+
     std::cout << (bsp(a,b,c,target_0) ? "true: the point is inside the triangle." : "false: the point is not inside the triangle.") << std::endl;
     std::cout << (bsp(a,b,c,target_1) ? "true: the point is inside the triangle." : "false: the point is not inside the triangle.") << std::endl;
+
+    target_1 = target_0;
+
+    std::cout <<"x = " << target_1.get_X() << "| y = " << target_1.get_Y() <<std::endl;
 
     return 0;
 }
