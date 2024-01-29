9f05395ad7e3dfe102bd88ac65fc49cccfbbf839
El Mehdi Bennix <ebennix@e1r8p12.1337.ma>
2024-01-29 18:19:06 +0100

    AutoPush

---
+++ CPP_04/ex01/Animal.cpp
@@ -0,0 +1,49 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Animal.cpp                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ebennix <ebennix@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/01/12 20:36:59 by ebennix           #+#    #+#             */
+/*   Updated: 2024/01/29 17:35:55 by ebennix          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Animal.hpp"
+
+Animal::Animal( void ) : type("")
+{
+    std::cout << "Animal default Constructor called." << std::endl;
+}
+
+Animal::Animal(const Animal& source)
+{
+    std::cout << "Animal copy Constructor called." << std::endl;
+    *this = source;
+}
+
+Animal&
+Animal::operator=(const Animal& source)
+{
+    if (this != &source)
+        this->type = source.type;
+    return (*this);
+}
+
+void
+Animal::makeSound( void ) const
+{
+    std::cout << "No Animal type." << std::endl;
+}
+
+std::string
+Animal::getType( void ) const
+{
+    return (this->type);
+}
+
+Animal::~Animal( void )
+{
+    std::cout << "Animal destructor called." << std::endl;
+}
