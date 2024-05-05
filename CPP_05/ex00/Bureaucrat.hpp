2cd63d29874ee8ec90769beb629da6a533485e57
El Mehdi Bennix <ebennix@e1r8p12.1337.ma>
2024-05-05 02:03:14 +0100

    AutoPush

--- CPP_05/ex00/Bureaucrat.hpp
+++ CPP_05/ex00/Bureaucrat.hpp
@@ -7,20 +7,115 @@
 #define HIGHEST_GRADE 1
 #define LOWEST_GRADE 150
 
+class GradeTooHighException : public std::exception
+{
+    private :
+        std::string _message;
+
+    public :
+        GradeTooHighException(const std::string& message) : _message(message) {};
+        const char* what() const noexcept {
+            return _message.c_str();
+        };
+};
+
+
+class GradeTooLowException : public std::exception
+{
+    private :
+        std::string _message;
+    
+    public :
+        GradeTooLowException(const std::string &message) : _message(message) {};
+        const char* what() const noexcept {
+            return _message.c_str();
+        };
+};
+
 class Bureaucrat
 {
     private:
         const std::string   _name;
         int                 _grade;
+
     public:
-        Bureaucrat(void);
-        Bureaucrat(std::string &name, int grade);
-        Bureaucrat(Bureaucrat &copy);
+        Bureaucrat(void) : _name("default"), _grade(150)
+        {
+            std::cout << "default constractor has been called" << std::endl;
+        };
 
+        Bureaucrat(const std::string& name, int grade) : _name(name)
+        {
+            std::cout << "not the constractor has been called" << std::endl;
+            if (grade < HIGHEST_GRADE)
+                throw GradeTooHighException("Bureaucrat::GradeTooHighException");
+            else if (grade > LOWEST_GRADE)
+                throw GradeTooLowException("Bureaucrat::GradeTooLowException");
+            this->_grade = grade;
+        };
 
-        
+        Bureaucrat(const Bureaucrat &copy)
+        {
+            *this = copy;
+            std::cout << "copy constractor has been called" << std::endl;
+        };
 
-        ~Bureaucrat(void);
+            // if we init bureau here and threw excep
+        Bureaucrat& operator=(const Bureaucrat &source) 
+        {
+            if (this != &source)
+            {
+                (std::string)this->_name = source._name;
+                this->_grade = source._grade;
+            }
+            return (*this);
+        }
+
+        const std::string& getName(void) const 
+        {
+            return _name;
+        };
+
+        int getGrade(void) const 
+        {
+            return _grade;
+        };
+
+        void setGrade(int grade)
+        {
+            if (grade < HIGHEST_GRADE)
+                throw GradeTooHighException("Bureaucrat::GradeTooHighException");
+            else if (grade > LOWEST_GRADE)
+                throw GradeTooLowException("Bureaucrat::GradeTooLowException");
+            this->_grade = grade;
+        };
+
+        int increment(void)
+        {
+            if (_grade == HIGHEST_GRADE)
+                throw GradeTooHighException("Bureaucrat::GradeTooHighException");
+            --_grade;
+            return _grade;
+        };
+
+        int decrement(void)
+        {
+            if (_grade == LOWEST_GRADE)
+                throw GradeTooLowException("Bureaucrat::GradeTooLowException");
+            ++_grade;
+            return _grade;
+        };
+
+        ~Bureaucrat(void)
+        {
+            std::cout << "distractor has benn called" << std::endl;
+        };
 };
+
+std::ostream& operator<<(std::ostream& os, const Bureaucrat& obj)
+{
+    os << obj.getName() << ", bureaucrat grade " << obj.getGrade();
+    return os;
+}
 
 #endif
