67593ed9377db5eafcbc50bd8d8ec5a8084c4fbf
ElmehdiBennix <bennixmehdi@gmail.com>
2024-05-05 02:03:14 +0100

    AutoPush

--- CPP_05/ex00/main.cpp
+++ CPP_05/ex00/main.cpp
@@ -3,5 +3,20 @@
 
 int main(void)
 {
-    
+    try
+    {
+       Bureaucrat e("nigga", 0);
+       std::cout << e << std::endl;
+    }
+    catch (Bureaucrat &e)
+    {
+        std::cout << "execption 1 " << std::endl;
+        std::cout << e << " bruhhhhh" << std::endl;
+    }
+    catch (std::exception & e)
+    {
+        std::cout << "execption 2 " << std::endl;
+        std::cout << e.what() << std::endl;
+    }
+    std::cout << "end" << std::endl;
 }
