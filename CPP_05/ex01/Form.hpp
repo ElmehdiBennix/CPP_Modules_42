f9e7337a77142dbd3500430b66877d3b118d0c0c
El Mehdi Bennix <ebennix@e1r8p12.1337.ma>
2024-05-11 02:50:30 +0100

    AutoPush

--- CPP_05/ex01/Form.hpp
+++ CPP_05/ex01/Form.hpp
@@ -1,1 +1,42 @@
-xxx
+#ifndef FORM_HPP__
+#define FORM_HPP__
+
+// #include <iostream>
+#include "Bureaucrat.hpp"
+
+class Form 
+{
+
+    private :
+        const std::string _name;
+        bool    _signator;
+        const int  _signatorGrade;
+        const int  _executionGrade;
+
+    public :
+        Form(void);
+        Form(const std::string& name);
+        Form(const Form& source);
+        Form& operator=(const Form& source);
+
+        const std::string&  getName(void) const;
+        bool                getSignator(void) const;
+        int                 getSignatorGrade(void) const;
+        int                 getExecutionGrade(void) const;
+
+        void beSigned(const Bureaucrat& obj);
+        void signForm(const Bureaucrat& obj);
+
+
+        ~Form(void);
+};
+        // void signForm(const Bureaucrat& obj);
+
+std::ostream& operator<<(std::ostream& os, const Form& ob)
+{
+    os << "tmp" ;
+    return os;
+}
+
+
+#endif
