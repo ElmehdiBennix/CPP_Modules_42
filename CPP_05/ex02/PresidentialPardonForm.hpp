c7ba12c396c41251f8ae28e7a5915cc5622a7241
ElmehdiBennix <bennixmehdi@gmail.com>
2024-05-17 19:58:49 +0100

    AutoPush

--- CPP_05/ex02/PresidentialPardonForm.hpp
+++ CPP_05/ex02/PresidentialPardonForm.hpp
@@ -1,1 +1,19 @@
-SSSS
+#ifndef PRESIDENTIALPARDONFORM_HPP__
+#define PRESIDENTIALPARDONFORM_HPP__
+
+#include "AForm.hpp"
+
+class PresidentialPardonFrom : public AForm {
+    private :
+
+    public :
+        PresidentialPardonFrom(void);
+        PresidentialPardonFrom();
+        PresidentialPardonFrom();
+
+
+
+        ~PresidentialPardonFrom();
+};
+
+#endif
--- CPP_05/ex02/RobotomyRequestFrom.hpp
+++ CPP_05/ex02/RobotomyRequestFrom.hpp
@@ -1,1 +1,19 @@
-SSS
+#ifndef ROBOTOMYREQUESTFORM_HPP__
+#define ROBOTOMYREQUESTFORM_HPP__
+
+#include "AForm.hpp"
+
+class RobotomyRequestForm : public AForm {
+    private :
+
+    public :
+        RobotomyRequestForm(void);
+        RobotomyRequestForm();
+        RobotomyRequestForm();
+
+
+
+        ~RobotomyRequestForm();
+};
+
+#endif
