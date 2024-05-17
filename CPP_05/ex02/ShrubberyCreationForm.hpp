ee44d0c173a0e1d5f3e8ec967549645b4fe013a1
root <root@Mehdi-PC>
2024-05-17 19:58:49 +0100

    AutoPush

--- CPP_05/ex02/ShrubberyCreationForm.hpp
+++ CPP_05/ex02/ShrubberyCreationForm.hpp
@@ -1,1 +1,27 @@
-SSS
+#ifndef SHRUBBERYCREATIONFORM_HPP__
+#define SHRUBBERYCREATIONFORM_HPP__
+
+#include "AForm.hpp"
+
+class ShrubberyCreationForm : public AForm {
+    private :
+
+    public :
+        ShrubberyCreationForm(void) : AForm("DEF", 145, 137) {
+            std::cout << "default constractor has been called." << std::endl;
+        };
+
+        ShrubberyCreationForm(const std::string &name) : AForm(name, 145, 137) {
+            std::cout << "constractor has been called." << std::endl;
+        };
+
+        ShrubberyCreationForm(const ShrubberyCreationForm& source) : AForm("DEF", 145, 137) {
+            std::cout << "copy constractor has been called." << std::endl;
+        };
+
+
+
+        ~ShrubberyCreationForm(void) throw();
+};
+
+#endif
