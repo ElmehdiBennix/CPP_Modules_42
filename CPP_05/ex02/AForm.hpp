704edeadc4705ccd56441c50e1b0047c290c4231
El Mehdi Bennix <ebennix@e1r3p2.1337.ma>
2024-05-18 01:54:07 +0100

    AutoPush

--- CPP_05/ex02/AForm.hpp
+++ CPP_05/ex02/AForm.hpp
@@ -1,8 +1,9 @@
 #ifndef AFORM_HPP__
 #define AFORM_HPP__
 
-// #include <iostream>
-#include "Bureaucrat.hpp"
+#include <iostream>
+
+class Bureaucrat;
 
 class AForm 
 {
@@ -13,80 +14,49 @@ class AForm
         const int          _executionGrade; // constant grade required to execute it.
 
     public :
-        AForm(void) : _name("DEF"), _signator(false), _signatorGrade(0), _executionGrade(0) {
-            std::cout << "Default constractor called." << std::endl;
-        };
-
-        AForm(const std::string& name, const int signatorGrade, const int executionGrade) : _name(name), _signator(false), _signatorGrade(signatorGrade), _executionGrade(executionGrade) {
-            std::cout << "Constractor called for Form" << std::endl;
-        };
-
-        // test this out after
-        AForm(const AForm& source) : _name(source._name), _signator(source._signator), _signatorGrade(source._signatorGrade), _executionGrade(source._executionGrade) {
-            std::cout << "Copy constractor called." << std::endl;
-        };
-
+        AForm(void);
+        AForm(const std::string& name, const int signatorGrade, const int executionGrade);
+    // test this out after
+        AForm(const AForm& source);
+        
         //#####################################################################
-//test this out
-        AForm& operator=(const AForm& source) {
-            AForm tmp(source);
-            return tmp;
-        };
+    
+    //test this out
+        // AForm& operator=(const AForm& source);
 
         //#####################################################################
 
-        const std::string&  getName(void) const throw() {
-            return _name;
-        };
+        const std::string&  getName(void) const throw();
+        bool    getSignator(void) const throw();
+        int     getSignatorGrade(void) const throw();
+        int     getExecutionGrade(void) const throw();
+        
+        //#####################################################################
 
-        bool    getSignator(void) const throw() {
-            return _signator;
-        };
-
-        int     getSignatorGrade(void) const throw() {
-            return _signatorGrade;
-        };
-
-        int     getExecutionGrade(void) const throw() {
-            return _executionGrade;
-        };
+        void beSigned(Bureaucrat& obj);
 
         //#####################################################################
 
-        void beSigned(const Bureaucrat& obj) {
-            if (obj.getGrade() <= _signatorGrade)
-                _signator = true;
-            throw (GradeTooLowException());
-        };
+        virtual void exec(const Bureaucrat& target) = 0;
 
         //#####################################################################
 
         class GradeTooHighException : public std::exception {
             public :
-                const char* what() const throw() {
-                    return ("AForm Grade is too low");
-                };
+                const char* what() const throw();
         };
 
         class GradeTooLowException : public std::exception {
             public :
-              const char* what() const throw() {
-                    return ("AForm Grade is too low");
-                };
+              const char* what() const throw();
         };
 
         //#####################################################################
 
-        ~AForm(void) throw() {
-            std::cout << "Destractor called for form." << std::endl;
-        };
+        ~AForm(void) throw();
 };
         // void signForm(const Bureaucrat& obj);
 
-std::ostream& operator<<(std::ostream& os, const AForm& obj)
-{
-    os << obj.getName() << " Form : \nGrade to be signed = " << obj.getSignatorGrade() << "\nGrade to be executed = " << obj.getExecutionGrade() << "\nSignator status : " << ((obj.getSignator()) ? "True" : "False");
-    return os;
-}
+std::ostream& operator<<(std::ostream& os, const AForm& obj);
 
 #endif
