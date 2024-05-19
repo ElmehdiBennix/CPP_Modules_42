d8db9a873e38ae37a713a21f4407ef78a51c35f5
El Mehdi Bennix <ebennix@e1r3p2.1337.ma>
2024-05-19 01:18:00 +0100

    AutoPush

--- CPP_05/ex02/PresidentialPardonForm.hpp
+++ CPP_05/ex02/PresidentialPardonForm.hpp
@@ -3,17 +3,30 @@
 
 #include "AForm.hpp"
 
-// class PresidentialPardonFrom : public AForm {
-//     private :
+class PresidentialPardonFrom : public AForm {
+    public :
+        PresidentialPardonFrom(void) : AForm("DEF", 25, 5) {
+            std::cout << "Default constractor called for PresidentialPardonFrom." << std::endl;
+        };
 
-//     public :
-//         PresidentialPardonFrom(void);
-//         PresidentialPardonFrom();
-//         PresidentialPardonFrom();
+        PresidentialPardonFrom(const std::string &name) : AForm(name, 25, 5) {
+            std::cout << "Constractor called for PresidentialPardonFrom." << std::endl;
+        };
 
+        // PresidentialPardonFrom(const PresidentialPardonFrom& source) : AForm("DEF", 25, 5) {
+        //     std::cout << "copy constractor has been called." << std::endl;
+        // };
 
+        void execute(const Bureaucrat& executor) const {
+            if (this->getSignator() && executor.getGrade() <= this->getExecutionGrade())
+            {
+                std::cout << executor.getName() << " has been pardoned by Zaphod Beeblebrox." << std::endl;
+                return ;
+            }
+            throw(GradeTooLowException());
+        };
 
-//         ~PresidentialPardonFrom();
-// };
+        ~PresidentialPardonFrom(void) {};
+};
 
 #endif
