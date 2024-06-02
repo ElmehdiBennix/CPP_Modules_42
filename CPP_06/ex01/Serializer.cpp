347c199e4fee145cc078596fc528133a1af59cc2
El Mehdi Bennix <ebennix@e1r3p1.1337.ma>
2024-06-03 00:06:04 +0100

    AutoPush

--- CPP_06/ex01/Serializer.cpp
+++ CPP_06/ex01/Serializer.cpp
@@ -1,6 +1,30 @@
 #include "Serializer.hpp"
 
+Serializer::Serializer(void)
+{};
+
+Serializer::Serializer(Serializer& source)
+{};
+
+Serializer& Serializer::operator=(Serializer& source)
+{};
+
+Serializer::~Serializer(void)
+{};
 
 
-        static uintptr_t serialize(Data* ptr);
-        static Data* Deserialize(uintptr_t ptr);
+
+uintptr_t Serializer::serialize(Data* ptr)
+{
+    // std::uintptr_t ret = 12;
+
+    // int *ptr = ret;
+
+    // ret = 
+};
+
+Data* Serializer::Deserialize(uintptr_t ptr)
+{
+        
+};
+
--- CPP_06/ex01/main.cpp
+++ CPP_06/ex01/main.cpp
@@ -2,5 +2,18 @@
 
 int main(void)
 {
-    
+    // Data x = {123456789};
+    Data *test = new Data;
+
+    test->testing = 1234566789;
+
+    std::cout << "before Serializer = " << test->raw << std::endl;
+    std::uintptr_t ptr = Serializer::serialize(test);
+    std::cout << "after Serialize = " << ptr << std::endl;
+
+    std::cout << "before deSerialize = " << ptr << std::endl;
+    test = Serializer::deserialize(ptr);
+    std::cout << "after deSerialize = " << test->raw << std::endl;
+
+    delete test;
 }
