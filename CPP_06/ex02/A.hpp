61f700890ce8d6ab4981afd6599633140bc859d5
El Mehdi Bennix <ebennix@e1r5p4.1337.ma>
2024-06-06 22:00:37 +0100

    AutoPush

---
+++ CPP_06/ex02/A.hpp
@@ -0,0 +1,14 @@
+#if !defined(A_CLASS_HPP__)
+#define A_CLASS_HPP__
+
+#include "Base.hpp"
+
+class A : public Base{
+    // private:
+        /* data */
+        // A(/* args */);
+    public:
+        virtual ~A(void);
+};
+
+#endif
