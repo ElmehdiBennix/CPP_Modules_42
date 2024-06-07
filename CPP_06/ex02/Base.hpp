d23fd2566ecac498716fcc783cbba75f0e72e586
ElmehdiBennix <bennixmehdi@gmail.com>
2024-06-07 18:02:35 +0100

    AutoPush

--- CPP_06/ex02/Base.hpp
+++ CPP_06/ex02/Base.hpp
@@ -2,17 +2,18 @@
 #define BASE_HPP__
 
 #include <iostream>
+#include <cstdlib>
 
-#define BASE_CLASS  0
-#define A_CLASS     1
-#define B_CLASS     2
-#define C_CLASS     3
+// #define BASE_CLASS  0
+#define A_CLASS     0
+#define B_CLASS     1
+#define C_CLASS     2
 
 class Base {
     // private:
 
     public:
-        virtual ~Base(void);
+        virtual ~Base(void){};
 };
 
 #endif // BASE_HPP__
