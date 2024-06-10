aaafd331851456ccf7f8f2d6b33d3a6fb13100b1
El Mehdi Bennix <ebennix@e1r10p12.1337.ma>
2024-06-10 23:39:09 +0100

    AutoPush

--- CPP_07/ex01/main.cpp
+++ CPP_07/ex01/main.cpp
@@ -1,7 +1,24 @@
 #include "iter.hpp"
 
+#define SIZE 5
+
 int main(void)
 {
+    size_t      size = 0;
 
-    
+    char str[SIZE] = {};
+    size = sizeof(str) / sizeof(str[0]);
+
+    iter(&str)
+
+    int num[SIZE] = {};
+    size = sizeof(num) / sizeof(num[0]);
+
+    double str[SIZE] = {};
+    size = sizeof(str) / sizeof(str[0]);
+
+    std::string str[SIZE] = {};
+    size = sizeof(str) / sizeof(str[0]);
+
+
 }
