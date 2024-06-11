804395d35fde2fda463c74f740ea75f5e7d5ea4b
El Mehdi Bennix <ebennix@e1r4p1.1337.ma>
2024-06-11 20:10:12 +0100

    AutoPush

--- CPP_07/ex01/main.cpp
+++ CPP_07/ex01/main.cpp
@@ -6,19 +6,36 @@ int main(void)
 {
     size_t      size = 0;
 
-    char str[SIZE] = {};
-    size = sizeof(str) / sizeof(str[0]);
+    char c[SIZE] = {'h', 'e', 'l','l','o'};
+    size = sizeof(c) / sizeof(c[0]);
+    iter(c , size, print<char>);
+    iter(c , size, decrement<char>);
+    std::cout << " |   ";
+    iter(c , size, print<char>);
+    std::cout << " |\n" << std::endl;
 
-    iter(&str)
-
-    int num[SIZE] = {};
+    int num[SIZE] = {1,2,3,4,5};
     size = sizeof(num) / sizeof(num[0]);
+    iter(num , size, print<int>);
+    iter(num , size, increment<int>);
+    std::cout << " |   ";
+    iter(num , size, print<int>);
+    std::cout << " |\n" << std::endl;
 
-    double str[SIZE] = {};
+    double doub[SIZE] = {1.2, 3.4, 4.5, 6.7, 8.9};
+    size = sizeof(doub) / sizeof(doub[0]);
+    iter(doub , size, print<double>);
+    iter(doub , size, decrement<double>);
+    std::cout << " |   ";
+    iter(doub , size, print<double>);
+    std::cout << " |\n" << std::endl;
+
+    std::string str[SIZE] = {"hello", "this", "is", "an", "array"};
     size = sizeof(str) / sizeof(str[0]);
-
-    std::string str[SIZE] = {};
-    size = sizeof(str) / sizeof(str[0]);
-
+    iter(str , size, print<std::string>);
+    iter(str , size, appand<std::string &>);
+    std::cout << " |   ";
+    iter(str , size, print<std::string>);
+    std::cout << " |\n" << std::endl;
 
 }
