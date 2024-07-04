493370da5f4afc64fcded51043a1f030349a11bf
ElmehdiBennix <bennixmehdi@gmail.com>
2024-07-04 05:31:22 +0100

    AutoPush

--- CPP_08/ex01/Span.hpp
+++ CPP_08/ex01/Span.hpp
@@ -5,7 +5,25 @@
 
 class Span
 {
-    
-}
+    private:
+        int *numbers;
+        
+
+    public:
+        Span(void);
+        Span(unsigned int N);
+        Span(Span &source);
+
+        void    addNumber(int num);
+        int     shortestSpan(void);
+        int     longestSpan(void);
+
+        ~Span(void)
+        {
+            std::cout << "distra is called" << std::endl;
+            delete[] numbers;
+        };
+
+};
 
 #endif
