b15dbff4f6f2c8b75bfe9de3973f1aa1273ce546
ElmehdiBennix <bennixmehdi@gmail.com>
2024-07-07 15:36:24 +0100

    AutoPush

--- CPP_08/ex01/Span.hpp
+++ CPP_08/ex01/Span.hpp
@@ -2,28 +2,54 @@
 #define SPAN_HPP__
 
 #include <iostream>
+#include <vector>
+#include <algorithm>
 
 class Span
 {
     private:
-        int *numbers;
-        
+        std::vector<int> numbers;
+        std::size_t size;
 
     public:
-        Span(void);
-        Span(unsigned int N);
-        Span(Span &source);
-
-        void    addNumber(int num);
-        int     shortestSpan(void);
-        int     longestSpan(void);
-
-        ~Span(void)
-        {
-            std::cout << "distra is called" << std::endl;
-            delete[] numbers;
+        Span(void) {
+            numbers.reserve(1);
+            size = 1;
+        };
+        Span(unsigned int N) {
+            numbers.reserve(N);
+            size = N;
+        };
+        Span(const Span &source) {
+            *this = source;
         };
 
+        Span& operator=(const Span &source) {
+            if (this != &source) {
+                numbers.resize(source.size);
+                std::copy(source.numbers.begin(), source.numbers.end(), this->numbers.begin());
+                size = source.size;
+            }
+            return (*this);
+        };
+
+        void    addNumber(int num) {
+            if (size > numbers.size())
+                std::cout << "passed" << std::endl;
+            numbers.push_back(num);
+        };
+
+        std::vector<int>* getvec(void){return &numbers;};
+
+        // int     shortestSpan(void) {
+
+        // };
+
+        // int     longestSpan(void) {
+
+        // };
+
+        ~Span(void) {};
 };
 
 #endif
--- CPP_08/ex01/main.cpp
+++ CPP_08/ex01/main.cpp
@@ -1,15 +1,27 @@
-
 #include "Span.hpp"
 
 int main()
 {
     Span sp = Span(5);
+
     sp.addNumber(6);
     sp.addNumber(3);
     sp.addNumber(17);
     sp.addNumber(9);
     sp.addNumber(11);
-    std::cout << sp.shortestSpan() << std::endl;
-    std::cout << sp.longestSpan() << std::endl;
+    sp.addNumber(12);
+
+    std::vector<int>* x = sp.getvec();
+    std::cout << "the size is = " << x->size() << std::endl;
+    std::cout << "the capacity is = " << x->capacity() << std::endl;
+
+    for (std::vector<int>::iterator it = x->begin(); it != x->end(); it++)
+    {
+        std::cout << *it << std::endl;
+    }
+
+    // std::cout << sp.shortestSpan() << std::endl;
+    // std::cout << sp.longestSpan() << std::endl;
+
     return 0;
 }
