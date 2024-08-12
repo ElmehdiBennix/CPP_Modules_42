e6779c6b1b5de54f0b03b370c59c4fdec449a071
ElmehdiBennix <bennixmehdi@gmail.com>
2024-08-12 07:58:08 +0100

    AutoPush

--- CPP_09/ex00/main.cpp
+++ CPP_09/ex00/main.cpp
@@ -1,8 +1,18 @@
 #include "BitcoinExchange.hpp"
 
-int main() {
+int main(int ac, char **av)
+{
+    if (ac != 2)
+        return (CWAR("Error: Usage ./btc <filepath>."), 1);
 
-
+    try {
+        Bitcoin data;
+        data.exchange(av[1]);
+    }
+    catch (std::exception &e) {
+        CERR(e.what());
+        return 2;
+    }
 
     return 0;
 }
