a5ccf8ae9c3c3e9552f04afb844dc046f627bdbb
ElmehdiBennix <bennixmehdi@gmail.com>
2024-09-07 20:20:19 +0100

    AutoPush

--- CPP_09/ex00/BitcoinExchange.cpp
+++ CPP_09/ex00/BitcoinExchange.cpp
@@ -12,11 +12,11 @@ Bitcoin::Bitcoin(void)
 
     std::ifstream  dataFile(DATA_FILE, std::ios_base::in);
     if (!dataFile.is_open())
-        throw (std::runtime_error("Error: Failed to open " DATA_FILE));
+        throw (std::runtime_error("Error: Failed to open " DATA_FILE "."));
 
     if (!parseCsv(dataFile)) {
         dataFile.close();
-        throw (std::runtime_error("Error: Failed to parse " DATA_FILE));
+        throw (std::runtime_error("Error: Failed to load database '" DATA_FILE "'"));
     }
 
     dataFile.close();
@@ -35,136 +35,157 @@ Bitcoin::operator=(const Bitcoin &source)
     return (*this);
 };
 
-bool
-Bitcoin::parseDate(const std::string &date)
+int
+Bitcoin::parseDate(const std::string &date) const
 {
-    if (date.length() != 10)
-        return (false);
-
-    if (date[5] != '-' || date[8] != '-')
-        return (false);
-
     std::istringstream tokens(date);
+
+    if (date.length() != 10)
+        return BADINPUT;
+
+
+    if (date[4] != '-' || date[7] != '-')
+        return BADINPUT;
 
     int  year, month, day;
     char dash[2];
 
     if (!(tokens >> year >> dash[0] >> month >> dash[1] >> day))
-        return (false);
+        return BADINPUT;
 
     if (dash[0] != '-' || dash[1] != '-')
-        return (false);
+        return BADINPUT;
 
-    if (1 > month || month > 12 || 1 > day || day < 31)
-        return (false);
+    if (1 > month || month > 12 || 1 > day || day > 31)
+        return BADINPUT;
 
     if ((month == 4 || month == 6 || month == 9 || month == 11) && day > 30)
-        return false;
+        return BADINPUT;
 
     // if (month == 2) {
     //     bool isLeapYear = (year % 4 == 0 && year % 100 != 0) || (year % 400 == 0);
     //     if (day > 29 || (day == 29 && !isLeapYear)) {
-    //         return false;
+    //         return BADINPUT;
     //     }
     // }
 
-    return (true);
+    std::string val(date);
+    val.erase(val.find_first_of('-'), 1);
+    val.erase(val.find_first_of('-'), 1);
+
+    return (atoi(val.c_str()));
 };
 
 float
-Bitcoin::parseValue(const std::string &date)
+Bitcoin::parseValue(const std::string &value, float min, float max) const
 {
     float number;
     char  remaining;
-    std::istringstream token(date);
-    
-    if (!(token >> number) || token >> remaining)
-        return false;
+    std::istringstream token(value);
 
-    if (number < 0 || number > 1000 || number != number || number == std::numeric_limits<float>::infinity() || number == -std::numeric_limits<float>::infinity())
+    if (!(token >> number) || token >> remaining)
+        return BADINPUT;
+
+    if (number < min)
+        return NEGATIVE;
+
+    if (number > max || number != number || number == std::numeric_limits<float>::infinity() || number == -std::numeric_limits<float>::infinity())
+        return OVERLIMIT;
+
+    return number;
+};
+
+bool
+Bitcoin::parseCsv(std::ifstream &dataFile)
+{
+    std::string line;
+
+    for (size_t i = 0; std::getline(dataFile, line); i++)
+    {
+        size_t pos = line.find(",");
+        if (pos == std::string::npos)
+            return false;
+
+        int   date = 0;
+        float rate = 0;
+        std::string lvalue = line.substr(0, pos);
+        std::string rvalue = line.substr(pos + 1);
+
+        if (i == 0)
+        {
+            if (lvalue != "date" || rvalue != "exchange_rate")
+                return false;
+        }
+        else
+        {
+            if ((date = parseDate(lvalue)) < 0 || (rate = parseValue(rvalue, 0, std::numeric_limits<float>::max())) < 0)
+                return false;
+        }
+        if (i != 0)
+            dataBase.insert(std::pair<int, float>(date , rate));
+    }
+    if (dataBase.empty())
         return false;
 
     return true;
 };
 
-bool
-Bitcoin::parseCsv(std::ifstream &dataFile) // dataFile.close();
+void
+Bitcoin::executeExchange(std::ifstream &inputFile) const
 {
-    size_t      i;
     std::string line;
 
-    for (size_t i = 0; std::getline(dataFile, line); i++)
+    for (size_t i = 0; std::getline(inputFile, line); i++)
     {
-        float rate = 0;
-        std::istringstream tokens(line);
-    
-        for (size_t j = 0; std::getline(tokens, line, ','); j++)
+        size_t pos = line.find(" | ");
+        if (pos == std::string::npos)
         {
-            if (i == 0) 
+            CERR("Error: bad input => " << line);
+            continue;
+        }
+
+        int   date = 0;
+        float rate = 0;
+        std::string lvalue = line.substr(0, pos);
+        std::string rvalue = line.substr(pos + 3);
+
+        if (i == 0)
+        {
+            if (lvalue != "date" || rvalue != "value")
+                CERR("Error: bad input => " << line);
+        }
+        else
+        {
+            if ((date = parseDate(lvalue)) < 0 || (rate = parseValue(rvalue, 0, 1000)) < 0)
             {
-                switch (j)
-                {
-                    case 0:
-                        if (line != "date")
-                            return (false);
-                        break;
-                    case 1:
-                        if (line != "exchange_rate")
-                            return (false);
-                        break;
-                    default:
-                        return (false);
-                }
+                if (date == BADINPUT || rate == BADINPUT)
+                    CERR("Error: bad input => " << line);
+                else if (date == OVERLIMIT)
+                    CERR("Error: too large a number.");
+                else
+                    CERR("Error: not a positive number.");
             }
             else
             {
-                switch (j)
-                {
-                    case 0:
-                        if (!parseDate(line))
-                            return (false);
-                        break;
-                    case 1:
-                        if ((rate = parseValue(line)) < 0)
-                            return (false);
-                        break;
-                    default:
-                        return (false);
-                }
+                std::map<int, float>::const_iterator it = dataBase.upper_bound(date);
+                --it;
+                COUT(lvalue << " => " << rvalue << " = " << rate * it->second);
             }
         }
-        if (i != 0)
-            dataBase.insert(std::pair<std::string, float>(line , rate));
     }
 
-    if (dataBase.empty())
-        return false;
-
-    return (true);
 };
-
-// bool
-// Bitcoin::readFile(std::ifstream& file, dataPoint &savePoint)
-// {
-    // std::string line;
-    // // std::stringstream token;
-    // // std::string tok;
-
-    // while (std::getline(file, line, '\n'))
-    // {
-    //     if (!parseCsv(line))
-    //         throw(std::runtime_error("Error : Failed to parse a valid csv format."));
-    // }
-
-
-    // return (true);
-// };
 
 void
 Bitcoin::exchange(const char *fileName) const
 {
-    (void)fileName;
-    return;
+    std::ifstream inputFile(fileName, std::ios_base::in);
+
+    if (!inputFile.is_open())
+        throw (std::runtime_error("Error: Failed to open " + std::string(fileName)));
+
+    executeExchange(inputFile);
+    inputFile.close();
 };
 
 Bitcoin::~Bitcoin(void)
