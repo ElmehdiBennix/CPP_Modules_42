b6498300ffceac381e7247f30a83825a0d809499
ElmehdiBennix <bennixmehdi@gmail.com>
2024-09-21 09:42:11 +0100

    AutoPush

--- CPP_09/ex01/RPN.hpp
+++ CPP_09/ex01/RPN.hpp
@@ -2,6 +2,7 @@
 #define RPN_HPP__
 
 #include <iostream>
+#include <sstream>
 #include <stack>
 
 #define RESET "\033[0m"
@@ -14,21 +15,23 @@
 #define CWAR(out) std::cout << YELLOW << out << RESET << std::endl
 #define CERR(out) std::cerr << RED << out << RESET << std::endl
 
+#define TOKENS(c)  ( c == '-' \
+                  || c == '+' \
+                  || c == '/' \
+                  || c == '*' )
+
 class RPN
 {
     private:
-        
-
-    public:
         RPN(void);
-        RPN(const std::string& expression);
         RPN(const RPN& source);
 
         RPN& operator=(const RPN& source);
 
-        // void
+        ~RPN(void);
 
-        ~RPN();
+    public:
+        static void calculatePostFix(const std::string& expression);
 };
 
 #endif
