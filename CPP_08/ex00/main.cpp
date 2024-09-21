1d579eb03fcccf8dde58e19819b2d26789578472
ElmehdiBennix <bennixmehdi@gmail.com>
2024-09-21 08:18:35 +0100

    AutoPush

--- CPP_08/ex00/main.cpp
+++ CPP_08/ex00/main.cpp
@@ -6,15 +6,17 @@ int main(void)
     int arr[] = {0, 1, 2, 3, 4, 5};
     std::vector<int> x(arr, arr + sizeof(arr) / sizeof(arr[0]));
 
-
-    try {
+    try
+    {
         std::vector<int>::iterator it = easyfind(x, 10);
         std::cout << "found a number : " << *it << std::endl;
 
-    } catch(std::exception &e) {
+    }
+    catch(const std::exception &e)
+    {
         std::cout << e.what() << std::endl;
     }
-    
+
     // for (std::vector<int>::iterator it = x.begin() ; )
     // std::vector<int>::iterator it = x.begin() ;
     // std::cout << *(++it) << std::endl;
--- CPP_08/ex01/Span.hpp
+++ CPP_08/ex01/Span.hpp
@@ -12,18 +12,25 @@ class Span
         std::size_t size;
 
     public:
-        Span(void) : size(1) {
+        Span(void) : size(1)
+        {
             numbers.reserve(1);
         };
-        Span(unsigned int N) : size(N){
+
+        Span(unsigned int N) : size(N)
+        {
             numbers.reserve(N);
         };
-        Span(const Span &source) {
+
+        Span(const Span &source)
+        {
             *this = source;
         };
 
-        Span& operator=(const Span &source) {
-            if (this != &source) {
+        Span& operator=(const Span &source)
+        {
+            if (this != &source)
+            {
                 numbers.resize(source.size);
                 std::copy(source.numbers.begin(), source.numbers.end(), this->numbers.begin());
                 size = source.size;
@@ -31,49 +38,58 @@ class Span
             return (*this);
         };
 
-        void    addNumber(int num) {
+        void    addNumber(int num)
+        {
             if (numbers.size() >= size)
                 throw (std::out_of_range("vector is full."));
             numbers.push_back(num);
         };
 
         template<typename it>
-        void    addNumber(it begin, it end) {
+        void    addNumber(it begin, it end)
+        {
             if (std::distance(begin, end) + numbers.size() >= size)
                 throw (std::out_of_range("vector is full."));
             numbers.insert(numbers.end(), begin, end);
         };
 
-        std::vector<int>* getvec(void){return &numbers;};
+        std::vector<int>* getvec(void)
+        {
+            return &numbers;
+        };
 
-    int shortestSpan(void) const {
-        if (numbers.size() < 2)
-            throw std::logic_error("Can't find the span. Fill in more numbers.");
+        int shortestSpan(void) const
+        {
+            if (numbers.size() < 2)
+                throw std::logic_error("Can't find the span. Fill in more numbers.");
 
-        std::vector<int> sorted_vec(numbers);
-        std::sort(sorted_vec.begin(), sorted_vec.end());
+            std::vector<int> sorted_vec(numbers);
+            std::sort(sorted_vec.begin(), sorted_vec.end());
 
-        int span = 2147483647;
+            int span = 2147483647;
 
-        for (std::vector<int>::const_iterator it = sorted_vec.begin() + 1; it != sorted_vec.end(); ++it) {
-            int tmp = *it - *(it - 1);
-            if (tmp < span)
-                span = tmp;
+            for (std::vector<int>::const_iterator it = sorted_vec.begin() + 1; it != sorted_vec.end(); ++it) {
+                int tmp = *it - *(it - 1);
+                if (tmp < span)
+                    span = tmp;
+            }
+            return span;
         }
-        return span;
-    }
 
-    int longestSpan(void) const {
-        if (numbers.size() < 2)
-            throw std::logic_error("Can't find the span. Fill in more numbers.");
+        int longestSpan(void) const
+        {
+            if (numbers.size() < 2)
+                throw std::logic_error("Can't find the span. Fill in more numbers.");
 
-        std::vector<int> sorted_vec(numbers);
-        std::sort(sorted_vec.begin(), sorted_vec.end());
+            std::vector<int> sorted_vec(numbers);
+            std::sort(sorted_vec.begin(), sorted_vec.end());
 
-        return (*(sorted_vec.end() - 1) - *sorted_vec.begin());
-    }
+            return (*(sorted_vec.end() - 1) - *sorted_vec.begin());
+        }
 
-    ~Span(void) {};
+        ~Span(void)
+        {
+        };
 };
 
 #endif
