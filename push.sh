f228b6d5c9ecef96607221cb56119e19464f7e3a
rambeau <bennixmehdi0@gmail.com>
2024-09-21 10:24:44 +0100

    AutoPush

--- push.sh
+++ push.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
 cd "$(git rev-parse --show-toplevel)"
-
-find . -type -d -exec bash -c 'cd "{}" && make fclean' \;
+# find . -type -d -exec bash -c 'cd "{}" && make fclean' \;
 
 git add .
 git status
