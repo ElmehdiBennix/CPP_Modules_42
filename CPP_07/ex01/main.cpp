f147718cf9b467aa620de4077f2bda79fd5c86fc
rambeau <bennixmehdi0@gmail.com>
2024-09-24 03:12:32 +0100

    AutoPush

--- CPP_07/ex01/main.cpp
+++ CPP_07/ex01/main.cpp
@@ -5,24 +5,30 @@ int main(void)
 {
     size_t      size = 0;
 
+    std::cout << "<======>" << std::endl;
+
     char c[SIZE] = {'h', 'e', 'l','l','o'};
     size = sizeof(c) / sizeof(c[0]);
+
     iter(c , size, print<char>);
-    std::cout << " |\n" << std::endl;
+    std::cout << "<======>" << std::endl;
 
     int num[SIZE] = {1,2,3,4,5};
     size = sizeof(num) / sizeof(num[0]);
+
     iter(num , size, print<int>);
-    std::cout << " |\n" << std::endl;
+    std::cout << "<======>" << std::endl;
 
     double doub[SIZE] = {1.2, 3.4, 4.5, 6.7, 8.9};
     size = sizeof(doub) / sizeof(doub[0]);
+
     iter(doub , size, print<double>);
-    std::cout << " |\n" << std::endl;
+    std::cout << "<======>" << std::endl;
 
     std::string str[SIZE] = {"hello", "this", "is", "an", "array"};
     size = sizeof(str) / sizeof(str[0]);
-    iter(str , size, print<std::string>);
-    std::cout << " |\n" << std::endl;
 
+    iter(str , size, print<std::string>);
+    std::cout << "<======>" << std::endl;
+    return 0;
 }
