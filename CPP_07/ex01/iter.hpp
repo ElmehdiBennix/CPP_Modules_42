3ff5d4a385dde15f5f8877027424d506d9eaa39d
ElmehdiBennix <bennixmehdi@gmail.com>
2024-09-24 03:04:13 +0100

    AutoPush

--- CPP_07/ex01/iter.hpp
+++ CPP_07/ex01/iter.hpp
@@ -3,8 +3,8 @@
 
 #include <iostream>
 
-template <typename T, typename F>
-void iter(T* addr, size_t len, F func)
+template <typename T>
+void iter(T* addr, size_t len, void (*func)(T))
 {
     for (size_t i = 0; i < len; i++)
         func(addr[i]);
@@ -14,24 +14,6 @@ template <typename T>
 void print(T type)
 {
     std::cout << " | " << type;
-}
-
-template <typename T>
-void increment(T& type)
-{
-    type++;
-}
-
-template <typename T>
-void decrement(T& type)
-{
-    type--;
-}
-
-template <typename T>
-void appand(T type)
-{
-    type = "_" + type + "_";
 }
 
 #endif
