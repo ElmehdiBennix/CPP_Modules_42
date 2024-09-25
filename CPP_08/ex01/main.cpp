391b7a0ee33c711a2e2f9df47fcdca000ad043ac
El Mehdi Bennix <ebennix@e1r3p3.1337.ma>
2024-09-25 23:27:18 +0100

    AutoPush

--- CPP_08/ex01/main.cpp
+++ CPP_08/ex01/main.cpp
@@ -1,10 +1,37 @@
 #include "Span.hpp"
 
+#define SIZE 50
+
 int main()
 {
     try
     {
-        Span sp;
+        srand(time(0));
+
+        std::vector<int> vec(SIZE);
+        std::cout << "====> starting print <====" << std::endl;
+        for (int i = 0; i < SIZE; i++)
+        {
+            vec[i] = rand();
+            std::cout << vec[i] << " ";
+        }
+        std::cout << std::endl;
+
+        Span holder(SIZE);
+        holder.addNumber(vec.begin(),vec.end());
+        const std::vector<int>& x = holder.getvec();
+
+        std::cout << "====> starting print <====" << std::endl;
+        for (int i = 0; i < SIZE; i++)
+            std::cout << x[i] << " ";
+        std::cout << std::endl;
+        std::cout << "<===================>" << std::endl;
+
+        std::cout << "shortest span : " << holder.shortestSpan() << std::endl;
+        std::cout << "longest span : " << holder.longestSpan() << std::endl;
+
+        std::cout << "<===================>" << std::endl;
+        Span sp = Span(6);
 
         sp.addNumber(6);
         sp.addNumber(3);
@@ -12,8 +39,9 @@ int main()
         sp.addNumber(9);
         sp.addNumber(11);
 
-        std::cout << sp.shortestSpan() << std::endl;
-        std::cout << sp.longestSpan() << std::endl;
+        std::cout << "shortest span : " << sp.shortestSpan() << std::endl;
+        std::cout << "longest span : " << sp.longestSpan() << std::endl;
+        std::cout << "<===================>" << std::endl;
     }
     catch(const std::exception& e)
     {
